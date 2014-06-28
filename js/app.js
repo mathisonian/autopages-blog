3161d94ad64b91304c0b0f12ada3a3a9abb3b3fc
Matthew Conlen <mc@mathisonian.com>
2014-06-28 12:51:57 -0400

    remove js

--- js/app.js
+++ js/app.js
@@ -8,37 +8,4 @@
 //
 
 
-var DesktopViewController = require('./views/desktop-controller');
-var MobileViewController = require('./views/mobile-controller');
-var emitter = require('./emitter');
-// var _ = require('lodash');
-
-var desktopView = null;
-var mobileView = null;
-var MOBILE_BREAKPOINT = 760;
-
-var $desktopEl = $('#desktopContent');
-var $mobileEl = $('#mobileContent');
-
-
-var draw = function() {
-    emitter.removeAllListeners();
-
-    if(desktopView) {
-        desktopView.destroy();
-    }
-    if(mobileView) {
-        mobileView.destroy();
-    }
-
-    if($(window).width() > MOBILE_BREAKPOINT) {
-        desktopView = new DesktopViewController($desktopEl);
-    } else {
-        mobileView = new MobileViewController($mobileView);
-    }
-};
-
-window.onresize = draw;
-draw();
-
 
