365f155177d6f8426cfb4018ff0d5da7de18ab9f
u18815 <u18815@ff3d6224-dd2a-42e2-8956-535787224061.(none)>
2014-08-03 22:01:35 +0000

    [Autopages] auto-creating new github pages.

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@
 //
 // app.js is the entry point for the entire client-side
 // application. Any necessary top level libraries should be
-// required here (e.g. pym.js), and it should also be
-// responsible for instantiating correct viewcontrollers.
+// required here, and it should also be
+// responsible for instantiating correct view controllers.
 //
 
 
